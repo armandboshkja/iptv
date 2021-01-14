a58dd3a9443bcc620237fb152fbc898f5bece7de
armandboshkja <77432503+armandboshkja@users.noreply.github.com>
2021-01-14 08:46:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+var time = new Date();
+
+fetch(`https://dailyiptvlist.com/dl/al-m3uplaylist-${time.getFullYear()}-${time.getMonth()}-${time.getDay()}-1.m3u`)
+.then(res => res.text())
+.then(body => {
+    if(body.includes('EXTM3U')){
+    fs.unlink('public/list.m3u', (err) =>{
+        if (err) return console.log(err);
+    })
+
+    fs.writeFile('public/list.m3u', body ,function (err) {
+        if (err) return console.log(err);
+    });
+    
+    }
+});
