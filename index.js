4c5699d1cdbc3fb0f22ac26995fdb4abcae494ba
armandboshkja <77432503+armandboshkja@users.noreply.github.com>
2021-01-14 09:06:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
-setInterval(mainFunction, 1680000);
+const express = require('express')
+const cors = require('cors');
 
-function mainFunction(){
-    var time = new Date();
+const app = express();
+var root = __dirname
 
-    fetch(`https://dailyiptvlist.com/dl/al-m3uplaylist-${time.getFullYear()}-${time.getMonth()}-${time.getDay()}-1.m3u`)
-    .then(res => res.text())
-    .then(body => {
-        if(body.includes('EXTM3U')){
-        fs.unlink('public/list.m3u', (err) =>{
-            if (err) return console.log(err);
-        })
+app.listen(process.env.PORT, () =>{
+  console.log('Server is working');
+});
 
-        fs.writeFile('public/list.m3u', body ,function (err) {
-            if (err) return console.log(err);
+app.use(cors());
+app.use(express.static(root + "/public"));
+
+
+app.get('/iptv' , async (req,res) => {
+        var time = new Date();
+
+        fetch(`https://dailyiptvlist.com/dl/al-m3uplaylist-${time.getFullYear()}-${time.getMonth()}-${time.getDay()}-1.m3u`)
+        .then(res => res.text())
+        .then(body => {
+            if(body.includes('EXTM3U')){
+            fs.unlink('public/list.m3u', (err) =>{
+                if (err) return console.log(err);
+            })
+
+            fs.writeFile('public/list.m3u', body ,function (err) {
+                if (err) return console.log(err);
+            });
+
+            }
         });
-
-        }
-    });
 }
