4e45141ee8825929ff33c13fab1396d77c5ae469
armandboshkja <77432503+armandboshkja@users.noreply.github.com>
2021-01-14 09:18:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
+const contentDisposition = require('content-disposition');
+
 const express = require('express')
 const cors = require('cors');
 
@@ -14,21 +16,32 @@ app.use(cors());
 app.use(express.static(root + "/public"));
 
 
-app.get('/iptv' , async (req,res) => {
+app.get('/update' , async (req,res) => {
         var time = new Date();
 
         fetch(`https://dailyiptvlist.com/dl/al-m3uplaylist-${time.getFullYear()}-${time.getMonth()}-${time.getDay()}-1.m3u`)
         .then(res => res.text())
         .then(body => {
             if(body.includes('EXTM3U')){
-            fs.unlink('public/list.m3u', (err) =>{
+            fs.unlink('list.m3u', (err) =>{
                 if (err) return console.log(err);
             })
 
-            fs.writeFile('public/list.m3u', body ,function (err) {
+            fs.writeFile('list.m3u', body ,function (err) {
                 if (err) return console.log(err);
             });
 
             }
         });
 }
+        
+        
+app.get('/iptv' , async (req,res) => {
+  res.writeHead(200, {
+          'Content-disposition': contentDisposition(`list.m3u`)});
+  
+  const fileStream = fs.createReadStream('list.m3u');
+  
+  fileStream.pipe(res);
+  
+})
