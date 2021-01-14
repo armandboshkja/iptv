dd0df8f7ba6547b6e308aa07b81e2f0046a640b6
armandboshkja <77432503+armandboshkja@users.noreply.github.com>
2021-01-14 08:59:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
+setInterval(mainFunction(), 1680000);
 
-var time = new Date();
+function mainFunction(){
+    var time = new Date();
 
-fetch(`https://dailyiptvlist.com/dl/al-m3uplaylist-${time.getFullYear()}-${time.getMonth()}-${time.getDay()}-1.m3u`)
-.then(res => res.text())
-.then(body => {
-    if(body.includes('EXTM3U')){
-    fs.unlink('public/list.m3u', (err) =>{
-        if (err) return console.log(err);
-    })
+    fetch(`https://dailyiptvlist.com/dl/al-m3uplaylist-${time.getFullYear()}-${time.getMonth()}-${time.getDay()}-1.m3u`)
+    .then(res => res.text())
+    .then(body => {
+        if(body.includes('EXTM3U')){
+        fs.unlink('public/list.m3u', (err) =>{
+            if (err) return console.log(err);
+        })
 
-    fs.writeFile('public/list.m3u', body ,function (err) {
-        if (err) return console.log(err);
+        fs.writeFile('public/list.m3u', body ,function (err) {
+            if (err) return console.log(err);
+        });
+
+        }
     });
-    
-    }
-});
+}
